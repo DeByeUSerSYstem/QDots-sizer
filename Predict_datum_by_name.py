d6c4990b6d4c88aefacf102792a03b888ca4f6ab
Lucia <82104975+goatworks@users.noreply.github.com>
2023-10-13 01:10:58 +0200

    added plot option

--- Predict_datum_by_name.py
+++ Predict_datum_by_name.py
@@ -2,12 +2,11 @@ import os
 # silence tensorflow I, E and W warnings
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'  # or any of {'0', '1', '3'} – 3 silences errors too
 import time
-from keras.models import load_model
-import numpy as np
 import argparse
+from glob import glob
+import numpy as np
 from utils import tools
-from utils import paths
-import sys
+from keras.models import load_model
 import matplotlib.pyplot as plt
 
 
@@ -15,13 +14,21 @@ def parse_args():
     """
     Parses input file(s) from command line
     """
-    parser = argparse.ArgumentParser(prog='5_process-real-datum_by-name.py',
-                                     description='Process real data with my CNN')
-    parser.add_argument('diffr', help='Relative Path to Diffractogram of interest')
-    parser.add_argument('-l', type=float, default=[0.56], help='Syncrhotron collection wavelength, in Å. DEFAULT=0.56')
-    parser.add_argument('--header_rows', type=int, default=0, help='Header rows before data starts')
+    parser = argparse.ArgumentParser(prog='Predict_datum_by_name.py',
+                    description='''Process experimental data with my CNN.\n
+                    Takes as arguments: full path to the diffractogram, the
+                    collection wavelength, and (if relevant) the number of
+                    rows to be skipped in the diffractogram file''')
+    parser.add_argument('diffr',
+                        help='Full or Relative path of the diffractogram')
+    parser.add_argument('-l', type=float, default=1.5406,
+                        help='Collection wavelength, in Å. If omitted, defaults to Kα1-Cu = 1.5406 Å')
+    parser.add_argument('--header_rows', type=int, default=0,
+                        help='Header rows before data starts. If omitted, all rows will be read')
+
     arg = parser.parse_args()
     return arg
+
 
 # --------------------------------------------
 #  PGM START
@@ -42,52 +49,81 @@ labels = np.arange(2.0, 10.1, 0.5)
 
 # set folders paths
 pattern_path = args.diffr
+pattern_dir = os.path.dirname(args.diffr)
 pattern_name = os.path.basename(args.diffr)
-test_data_dir = os.path.dirname(args.diffr)
-model_dir = os.path.join(paths.project_dir, 'good-models')
+name_no_ext = os.path.splitext(pattern_name)[0]
 
 # load model
-modelpath = os.path.join(model_dir, 'mix', 'Step05_Q14_R_MIX_1.h5')
-model = load_model(modelpath , compile=True)
+modelpath = glob('*.h5')[0]
+model = load_model(modelpath, compile=True)
 
 
 # --------------------------------------------
 #  LOAD, PREPROCESS AND PREDICT
 # --------------------------------------------
-#pattern = 'PbSOA_TOL_8nm_0p8Qz_E22_T17keV_SAMPLE_CORR.xye'
+# pattern = 'PbSOA_TOL_8nm_0p8Qz_E22_T17keV_SAMPLE_CORR.xye'
 print(f'\nOriginal filename: {pattern_name}')
+print(f'Using model {modelpath}\n')
 
 # load test data x(2θ) and y(I)
-x_real, y_real = np.loadtxt(pattern_path, skiprows=args.header_rows, usecols=(0, 1), unpack=True)
+x_exp, y_exp = np.loadtxt(pattern_path, skiprows=args.header_rows,
+                          usecols=(0, 1), unpack=True)
 
 # convert 2θ to Q
-q_real = tools.tt2q(x_real, args.l)
+q_exp = tools.tt2q(x_exp, args.l)
 
-# select desired Q values
-if np.amin(q_real) <= qmin and np.amax(q_real) >= qMAX:
-    idx = np.where((q_real > qmin) & (q_real < qMAX))
+# --- select desired Q values
+# if experimental pattern Q-range is same or wider than the std settings:
+if np.amin(q_exp) <= qmin and np.amax(q_exp) >= qMAX:
+    idx = np.where((q_exp >= qmin) & (q_exp <= qMAX))
+
+    # spline interpolation of test data in the just selected Q-range
+    x = np.arange(qmin, qMAX, 0.0028)
+    y = np.interp(x, q_exp[idx], y_exp[idx])
+
+    # normalize plot to an area under the curve (auc) = 1000
+    auc = np.trapz(y, x=x)
+    y_norm = y * (1000/auc)
+    y_final = y_norm
+
+# if the pattern Q-range is narrower than the std settings:
 else:
-    print('Data Q-range shorter than allowed (14)')
-    sys.exit(1)
+    qmin_exp = q_exp[0] if q_exp[0] >= qmin else qmin
+    qMAX_exp = q_exp[-1] if q_exp[-1] <= qMAX else qMAX
+    idx = np.where((q_exp >= qmin_exp) & (q_exp <= qMAX_exp))
 
-# spline interpolation of test data in the just selected Q-range
-x = np.arange(qmin, qMAX, 0.0028)
-y = np.interp(x, q_real[idx], y_real[idx])
+    # spline interpolation of test data in the just selected Q-range
+    x_short = np.arange(qmin_exp, qMAX_exp, 0.0028)
+    y = np.interp(x_short, q_exp[idx], y_exp[idx])
 
-# normalize plot to and area under the curve (auc) = 1000
-auc = np.trapz(y, x=x)
-y_norm = y * (1000/auc)
+    # normalize plot to an area under the curve (auc) = 1000
+    auc = np.trapz(y, x=x_short)
+    y_norm = y * (1000/auc)
+
+    # pad pattern with as many zeros as to reach the std settings
+    x = np.arange(qmin, qMAX, 0.0028)
+    npoints_pre = len(x[x < qmin_exp])
+    npoints_post = 5004 - (npoints_pre + len(y_norm))
+    # create 2 arrays of zeros of the correct lenghts, and PRE- & POST- append them
+    pre_pad = np.zeros((npoints_pre,))
+    post_pad = np.zeros((npoints_post,))
+    y_final = np.concatenate((pre_pad, y_norm, post_pad), axis=None)
+    assert len(y_final) == 5004
+
 
 # save normalised plot as .xy
-#norm_pattern = np.hstack((x.reshape(-1, 1), y_norm.reshape(-1, 1)))
-#np.savetxt(os.path.join(test_data_dir, 'pattern_normalised'), norm_pattern, fmt='%.15f')
+norm_pattern = np.hstack((x.reshape(-1, 1), y_final.reshape(-1, 1)))
+norm_pattern_path = os.path.join(pattern_dir, f'{name_no_ext}_norm.xy')
+np.savetxt(norm_pattern_path, norm_pattern, fmt='%.15f')
 
 # generate prediction of test data
-prediction = model.predict(y_norm.reshape(1, -1))
+prediction = model.predict(y_final.reshape(1, -1))
 
 # get class number and link to relative label
-class_n = np.argmax(prediction)  # axis=1, if multiple test data
+class_n = np.argmax(prediction)  # add axis=1, if multiple test data
 class_lbl = labels[class_n]
+
+b = time.time()  # time check
 
 
 # --------------------------------------------
@@ -95,12 +131,12 @@ class_lbl = labels[class_n]
 # --------------------------------------------
 
 # print result
-print(f'Predicted size: {class_lbl} nm')
+print(f'Predicted size: {class_lbl} nm\n')
 
 # plot
-fig, ax = plt.subplots(figsize=(15, 7))
+fig, ax = plt.subplots(figsize=(12, 6))
 ax.set_title(f'Predicted size: {class_lbl} nm')
-ax.plot(x, y_norm, color='xkcd:teal', label=pattern_name)
+ax.plot(x, y_final, color='xkcd:teal', label=pattern_name)
 ax.set_xlabel(r'$Q\ (Å^{-1})$')
 ax.set_ylabel('Intensitiy (a.u.)')
 ax.set_yticks([])
@@ -108,16 +144,22 @@ ax.legend(loc='upper right')
 fig.tight_layout()
 
 # Input user on saving plot
-affermative = ['y', 'yes', 'yeah', 'yup']
+affermative_answer = ['y', 'yes', 'yeah', 'yup']
+fname_frendly_lbl = 'p'.join(str(class_lbl).split('.'))
+
 choice = input('Would you like to save the plot? [Y/n]:  ')
-if choice.lower() in affermative or choice is None:
-	print(f'Plot will be saved in the same folder as the original under the name "{pattern_name}_PredAvgSize{class_lbl}"')
-	folder_choice = input('Would you like to change folder? [Y/n]:  ')
-	if folder_choice.lower() in affermative:
-		user_folder = input('Please, specify Relative Path here:  ')
-		plt.savefig(os.path.join(user_folder, f'{pattern_name[:-4]}_PredictedSize{class_lbl}.png'), dpi=200)
-	else:
-		plt.savefig(os.path.join(test_data_dir, f'{pattern_name[:-4]}_PredictedSize{class_lbl}.png'), dpi=200)
+if choice.lower() in affermative_answer or choice == '':
+    print(f'Plot will be saved in the same folder as the original file, under the name "{name_no_ext}_PredictedSize{fname_frendly_lbl}"')
+    folder_choice = input('Would you like to change folder? [y/N]:  ')
+    if folder_choice.lower() in affermative_answer:
+        user_folder = input('Please, specify new Relative Path here (folder must already exist):  ')
+        plt.savefig(os.path.join(user_folder,
+                    f'{name_no_ext}_PredictedSize{fname_frendly_lbl}.png'), dpi=200)
+    else:
+        plt.savefig(os.path.join(pattern_dir,
+                    f'{name_no_ext}_PredictedSize{fname_frendly_lbl}.png'), dpi=200)
+    plt.show()
+
 # else, just show it
 else:
     print('Just showing plot')
@@ -125,6 +167,4 @@ else:
 
 
 # feedback
-b = time.time()  # time check
-print(f'done in {round(b-a)}sec ✓')
-
+print(f'done in {round(b-a)}sec ✓\n')
